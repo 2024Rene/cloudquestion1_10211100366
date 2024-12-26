0bc9eab91b43cf298574f80b2c0434abcff97a0b
2024Rene <rene.alordeh-heymann@acity.edu.gh>
2024-12-26 22:45:38 +0000

    bypass

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -3,16 +3,23 @@ import { notFound } from "next/navigation"
 import Stripe from "stripe"
 import { CheckoutForm } from "./_components/CheckoutForm"
 
+// Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
-export default async function PurchasePage({
-  params: { id },
-}: {
-  params: { id: string }
-}) {
+type PageProps = {
+  params: {
+    id: string
+  }
+}
+
+export default async function PurchasePage({ params }: PageProps) {
+  const { id } = params // destructure to get the product ID
+
+  // Fetch product details from the database
   const product = await db.product.findUnique({ where: { id } })
   if (product == null) return notFound()
 
+  // Create Stripe payment intent
   const paymentIntent = await stripe.paymentIntents.create({
     amount: product.priceInCents,
     currency: "USD",
